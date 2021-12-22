dd36ba65e9214dc9b73db45b259128bda9e70288
Victor Hugo Amaral Santana <victorhugoamaralsantana@gmail.com>
2021-12-22 16:10:09 -0300

    Bootstrap Implementation

--- generator/views.py
+++ generator/views.py
@@ -7,9 +7,6 @@ import random
 def home(request):
     return render(request, 'generator/home.html', {'password': 'aduh123jn512i'})
 
-def about(request):
-    return render(request, 'generator/about.html')
-
 def password(request):
     thepassword = ''
     
